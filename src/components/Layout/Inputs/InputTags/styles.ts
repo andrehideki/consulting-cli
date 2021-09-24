e105bc9a02d74f7b57fba8aecc81e4071d18c45d
andrehideki <andre.hkiw@gmail.com>
2021-09-24 10:07:11 -0300

    style: change tags input style

--- src/components/Layout/Inputs/InputTags/styles.ts
+++ src/components/Layout/Inputs/InputTags/styles.ts
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
 
 export const PossibleTags = styled.div`
-  position: fixed;
+  position: absolute;
   margin-top: 1.5rem;
+  left: 0;
+  right: 0;
   width: auto;
   background: var(--color-white);
-  border: 1px solid;
+  border: 1px solid var(--color-border);
+  border-radius: 6px;
+  z-index: 99;
 `;
 
 export const PossibleTag = styled.div`
-  
+  font-size: 1rem;
+  padding: .25rem;
+  background: var(--color-white);
+  border-top: 1px solid var(--color-border);
 `;
